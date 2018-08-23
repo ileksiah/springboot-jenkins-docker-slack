84f093e459b8b38db6526f8047443cfd0733d287
ileksiah <ileksiah@gmail.com>
2018-08-23 14:19:48 +0900

    git-hook 테스트

--- src/main/java/com/blogcode/Application.java
+++ src/main/java/com/blogcode/Application.java
@@ -6,7 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
+	//test
 	public static void main(String[] args) {
+		System.out.print("java test");
 		SpringApplication.run(Application.class, args);
 	}
 }
