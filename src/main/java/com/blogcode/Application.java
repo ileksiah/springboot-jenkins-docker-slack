d93a321687d7e31108950b127cc5f4c5e025a7b7
ileksiah <ileksiah@gmail.com>
2018-08-23 15:20:08 +0900

    git-hook 테스트3

--- src/main/java/com/blogcode/Application.java
+++ src/main/java/com/blogcode/Application.java
@@ -8,8 +8,9 @@ public class Application {
 
 	//test
 	public static void main(String[] args) {
-		System.out.print("java test----");
+		System.out.print("java test----1");
 		SpringApplication.run(Application.class, args);
-        System.out.print("java test----");
+        System.out.print("java test----2");
+        System.out.print("java test----3");
 	}
 }
